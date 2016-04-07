ee75ec470ab598dc9deaa6414be51809aac95f76
Translator5 <wulfwolf1@msn.com>
2016-04-07 16:04:25 +0200

    Endless Legend with Wine 1.9.7
    de.v 0.9

--- EndlessLegend.sh
+++ EndlessLegend.sh
@@ -10,32 +10,21 @@
 [ "$PLAYONLINUX" = "" ] && exit 0
 source "$PLAYONLINUX/lib/sources"
 
-TITLE="SMITE"
-PREFIX="Smite"
+TITLE="Endless Legend"
+PREFIX="ENDLESSLEGEND"
 WINEVERSION="1.9.7"
 
 POL_SetupWindow_Init
 POL_Debug_Init
 
-POL_SetupWindow_presentation "$TITLE" "Hi-Rez Studios" "http://www.smitegame.com/" "Rolando Islas" "Smite"
+POL_SetupWindow_presentation "$TITLE" "Amplitude Studio" "http://www.endless-legend.com/" "Translator5" "Endless Legend"
 
-POL_SetupWindow_InstallMethod "DOWNLOAD,LOCAL"
+POL_SetupWindow_InstallMethod "LOCAL"
 
-if [ "$INSTALL_METHOD" = "LOCAL" ]; then
+	[ "$INSTALL_METHOD" = "LOCAL" ]; then
 	cd "$HOME"
 	POL_SetupWindow_browse "$(eval_gettext 'Please select the setup file to run.')" "$TITLE" "" "Windows Executables (*.exe)|*.exe;*.EXE"
 	FULL_INSTALLER="$APP_ANSWER"
-else
-	POL_System_TmpCreate "$PREFIX"
- 
-	DOWNLOAD_URL="http://hirez.http.internapcdn.net/hirez/InstallSmite.exe"
-	DOWNLOAD_MD5="9cdc39efb3c26e5d10b023d5a015ff7e"
-	DOWNLOAD_FILE="$POL_System_TmpDir/$(basename "$DOWNLOAD_URL")"
- 
-	POL_Call POL_Download_retry "$DOWNLOAD_URL" "$DOWNLOAD_FILE" "$DOWNLOAD_MD5" "$TITLE installer"
- 
-	FULL_INSTALLER="$DOWNLOAD_FILE"
-fi
 
 POL_System_SetArch "x86"
 POL_Wine_SelectPrefix "$PREFIX"
@@ -45,17 +34,9 @@ if [ "$POL_OS" = "Linux" ]; then
 	POL_Call POL_Function_RootCommand "echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope; exit"
 fi
 
-POL_Call POL_Install_d3dx10
-POL_Call POL_Install_d3dx11
-POL_Call POL_Install_d3dx9_43
-POL_Call POL_Install_directx9
 POL_Call POL_Install_dotnet35sp1
 POL_Call POL_Install_dotnet40
-POL_Call POL_Install_flashplayer
-POL_Call POL_Install_gdiplus
-POL_Call POL_Install_vcrun2008
 POL_Call POL_Install_vcrun2010
-POL_Call POL_Install_xact
 
 Set_OS "win7"
 
@@ -65,11 +46,8 @@ POL_Wine "$FULL_INSTALLER"
 Set_OS "winxp"
 
 POL_Call POL_Function_OverrideDLL builtin,native dnsapi
-POL_Shortcut "HiRezLauncherUI.exe" "$TITLE" "" "game=300 product=17"
-
-if [ "$INSTALL_METHOD" = "DOWNLOAD" ]; then
-	POL_System_TmpDelete
-fi
+POL_Shortcut "Launcher.exe" "$TITLE" "" ""
+POL_Shortcut "EndlessLegend.exe" "Play Endless Legend" "" ""
 
 POL_SetupWindow_Close
 exit
